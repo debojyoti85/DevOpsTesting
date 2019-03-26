0f1c305fc4f8d3d0bf410323bfb0dbada06f2cc0
Mallick <debojyoti.mallick@accenture.com>
2019-03-26 12:53:52 +0530

    Added files

---
+++ src/test/java/MavenProject/MavenProject/NewTest.java
@@ -0,0 +1,11 @@
+package MavenProject.MavenProject;
+
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  public void f() {
+	  
+	  System.out.println("This is testing");
+  }
+}
