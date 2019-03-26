ad27bd80d36ad83e41d98170f4c6ecd2901b80d7
Mallick <debojyoti.mallick@accenture.com>
2019-03-26 14:48:38 +0530

    Adding Selenium files

--- src/test/java/MavenProject/MavenProject/NewTest.java
+++ src/test/java/MavenProject/MavenProject/NewTest.java
@@ -1,11 +1,28 @@
 package MavenProject.MavenProject;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class NewTest {
   @Test
   public void f() {
 	  
-	  System.out.println("This is testing");
+	 
+	  System.setProperty("webdriver.chrome.driver","C:\\Users\\debojyoti.mallick\\Downloads\\chromedriver_win32\\chromedriver.exe");
+	  //launch chrome browser
+	  WebDriver driver=new ChromeDriver();
+	  //open the application
+	  driver.get("http://newtours.demoaut.com/");
+	  
+	  //enter username
+	  driver.findElement(By.name("userName")).sendKeys("mercury");
+	  driver.findElement(By.name("password")).sendKeys("mercury");
+	  driver.findElement(By.name("login")).click();
+	  driver.close();
   }
 }
+
+
+
